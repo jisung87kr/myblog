e993413a7aae63b2f695283d467fd339f38157e6
jisung <jisung87kr@nate.com>
2019-03-05 01:13:37 +0900

    make list page

--- mysite/myblog/models.py
+++ mysite/myblog/models.py
@@ -6,8 +6,8 @@ class Post(models.Model):
     autor = models.ForeignKey('auth.User', on_delete=models.CASCADE)
     title = models.CharField(max_length=255, null=False, blank=False)
     content = models.TextField()
-    created_date = models.DateTimeField(timezone.now)
-    published_date = models.DateTimeField(timezone.now)
+    created_date = models.DateTimeField(default=timezone.now)
+    published_date = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
         return self.title
--- mysite/myblog/views.py
+++ mysite/myblog/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from django.utils import timezone
+from .models import Post
+
 # Create your views here.
 def index(requests):
-    return HttpResponse('hello world!')
+    posts = Post.objects.filter(published_date__lte = timezone.now())
+    return render(requests, 'myblog/list.html', {'posts' : posts})
